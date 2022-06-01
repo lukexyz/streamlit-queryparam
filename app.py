269330c8f9c98c035c68113352e539c92e0a91e1
Luke <lukedwoods@gmail.com>
2022-06-01 14:56:32 +0000

    add user query param

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ st.header('`Streamlit-queryparams` 🌐')
 
 # 1. Get params
 params = st.experimental_get_query_params()
-st.write("Active query params:")
-st.write(params)
 
 # 2. Coordinate
 # init session variables
@@ -14,12 +12,19 @@ if "is_checked" not in st.session_state:
         params.get("is_checked", ["False"])[0].lower() == "true"
     )
 
+if "name" not in st.session_state:
+    st.session_state["name"] = 'User1'
+if params.get("name"):
+    st.session_state["name"] = params.get("name")[0]
+
+
+st.write(f"Hello `{st.session_state['name']}`, these are your URL parameters:")
+st.write(params)
+
 checker = st.checkbox("Checkbox",
                       key="is_checked")
 
-st.write(checker)
+st.write('Checked?', st.session_state["is_checked"])
 
-st.write(st.session_state["is_checked"])
-
-#3. Set params
-st.experimental_set_query_params(is_checked=checker)
+# 3. Set params
+st.experimental_set_query_params(is_checked=checker, name=st.session_state["name"])
